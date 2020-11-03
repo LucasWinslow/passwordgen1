03b08eb80f410624daec157a6342578af2ac8501
Lucas W <winslowlucas8@gmail.com>
2020-11-02 20:17:38 -0500

    html edited, README edited

--- pass.js
+++ pass.js
@@ -14,7 +14,7 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+// connects buttons to prompts and displays first promp
 generateBtn.addEventListener("click", getLength);
 function getLength() {
     length = parseInt(prompt("how many characters do you want"));
@@ -31,12 +31,12 @@ function getLength() {
 
 function generatePassword() {
    
-    
+    // list of possible values for password as var in array and strings
     var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
     var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
     var characters = "!@#$%^&*()_-+=[{]};:'<,>./?";
     var numbers = "0123456789";
-    
+    // asks user if they want lower case letters, if so adds it to possible answers and if not does not add.
     console.log('test')
     if (confirm("Would you like lower case letters?")) {
         charOptions.push(lowerCase)
@@ -46,7 +46,7 @@ function generatePassword() {
         makePass()
     }
    
-    
+    //asks user if they want upper case letters if so adds, if not does not add to possible password 
     console.log('test1')
     if (confirm ("would you like upper case letters?")){
         charOptions.push(upperCase)
@@ -56,14 +56,14 @@ function generatePassword() {
         makePass()
     }
     
-    
+    //  asks user if they want numbers.
     if(confirm("Would you like to add some numbers?")){
         charOptions.push(numbers)
     }
     if (charOptions.length !==0){
         makePass()
     }
-    
+    //  asks user if they want any special characters.
     if(confirm("Would you like to add any special characters? ")){
         charOptions.push(characters)
         
@@ -72,7 +72,7 @@ function generatePassword() {
         makePass()
     }
     
-
+// removes any commas and places a blank space. also adds another prompt with random password 
     var result = ""; 
     for (var i = 0; i < pass.length; i++){
         result = result.concat(pass[i]);
@@ -80,7 +80,7 @@ function generatePassword() {
 
     if(confirm("Your random password is:     " + result) );
 }
-
+// this is a for loop that runs through the user answered questions and filters what the user wishes to be in their password.
 function makePass(){
      pass =[]
     for (var i = 0; i < length ; i++) {
